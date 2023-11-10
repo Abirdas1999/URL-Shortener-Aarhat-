6bbadc8a206f781c500fa8ffb0e01983a57cb2ee
Abirdas1999 <111376255+Abirdas1999@users.noreply.github.com>
2023-11-10 16:40:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function shortenUrl() {
+    const longUrlInput = document.getElementById('longUrlInput');
+    const shortUrlDisplay = document.getElementById('shortUrl');
+
+    
+    const shortUrl = "http://short.url/abc123";
+
+    
+}
